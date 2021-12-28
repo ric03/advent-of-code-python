e924e1cb0362a291a429813df2b77137e34ade85
Tom Schneider <ric0.github@gmail.com>
2021-12-28 23:02:15 +0100

    feat: 2021-01 part two

--- 2021-01/2021_01.py
+++ 2021-01/2021_01.py
@@ -19,6 +19,14 @@ def count_increases(measurements):
     return count_of_increases
 
 
+def group_measurements(values):
+    new_value_list = []
+    for i in range(len(values) - 2):
+        _sum = values[i] + values[i + 1] + values[i + 2]
+        new_value_list.append(_sum)
+    return new_value_list
+
+
 def part_one(filename):
     measurements = read_numbers(filename)
     return count_increases(measurements)
@@ -26,25 +34,25 @@ def part_one(filename):
 
 def part_two(filename):
     measurements = read_numbers(filename)
-    return count_increases(measurements)
+    grouped_measurements = group_measurements(measurements)
+    return count_increases(grouped_measurements)
 
 
 class PartOne(unittest.TestCase):
     def test_example(self):
-        self.assertEqual(part_one("2021_01_example.txt"), 7)
+        self.assertEqual(7, part_one("2021_01_example.txt"))
 
     def test_puzzle(self):
-        self.assertEqual(part_one("2021_01_puzzle.txt"), 1475)
+        self.assertEqual(1475, part_one("2021_01_puzzle.txt"))
 
 
 class PartTwo(unittest.TestCase):
     def test_example(self):
-        self.assertEqual(part_two("2021_01_example.txt"), 5)
+        self.assertEqual(5, part_two("2021_01_example.txt"))
 
     def test_puzzle(self):
-        self.assertEqual(part_two("2021_01_puzzle.txt"), -1)
+        self.assertEqual(1516, part_two("2021_01_puzzle.txt"))
 
 
 if __name__ == '__main__':
     unittest.main()
-
